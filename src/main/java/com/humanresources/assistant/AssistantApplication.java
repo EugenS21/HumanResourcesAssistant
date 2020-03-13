3d77046b5ff3cf327c3806329ef5f9de49f21000
esavca <eugeniu.savca@endava.com>
2020-03-13 13:17:17 +0200

    Removed web;

--- src/main/java/com/humanresources/assistant/AssistantApplication.java
+++ src/main/java/com/humanresources/assistant/AssistantApplication.java
@@ -1,6 +1,5 @@
 package com.humanresources.assistant;
 
-import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.ApplicationPidFileWriter;
@@ -8,7 +7,7 @@ import org.springframework.boot.context.ApplicationPidFileWriter;
 @SpringBootApplication
 public class AssistantApplication {
 	public static void main(String[] args) {
-		SpringApplicationBuilder app = new SpringApplicationBuilder(AssistantApplication.class).web(WebApplicationType.NONE);
+		SpringApplicationBuilder app = new SpringApplicationBuilder(AssistantApplication.class);
 		app.build().addListeners(new ApplicationPidFileWriter("./bin/shutdown.pid"));
 		app.run();
 	}
