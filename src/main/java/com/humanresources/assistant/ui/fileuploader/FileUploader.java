1687349927512d2d610ec4deed6b74558ef01849
Eugen <savcaeugen@gmail.com>
2020-03-14 09:25:45 +0200

    Adding `FileUploader` class;

--- src/main/java/com/humanresources/assistant/ui/fileuploader/FileUploader.java
+++ src/main/java/com/humanresources/assistant/ui/fileuploader/FileUploader.java
@@ -1,38 +1,56 @@
 package com.humanresources.assistant.ui.fileuploader;
 
 import com.humanresources.assistant.ui.MainLayout;
+import com.vaadin.flow.component.Text;
+import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.html.Label;
-import com.vaadin.flow.component.html.Paragraph;
 import com.vaadin.flow.component.html.Span;
-import com.vaadin.flow.component.icon.VaadinIcon;
-import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.upload.Upload;
 import com.vaadin.flow.component.upload.receivers.MemoryBuffer;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 
+import static com.vaadin.flow.component.button.ButtonVariant.MATERIAL_CONTAINED;
+
 @Route(value = "FilesUploader", layout = MainLayout.class)
 @PageTitle("Upload the CV you want to be analyzed")
-public class FileUploader extends HorizontalLayout {
+public class FileUploader extends VerticalLayout {
 
     public static final String VIEW_NAME = "File Uploader";
-    private MemoryBuffer buffer = new MemoryBuffer();
-    private Upload upload = new Upload(buffer);
+    private final MemoryBuffer memoryBuffer;
+    private final Upload uploadContainer;
+    private final Button uploadButton;
+    private final Span informationContainer;
 
     public FileUploader() {
-        add(VaadinIcon.DIPLOMA.create());
-        add(new Span(" Here should be a list of CVs which were sent by people "));
         setSizeFull();
         setJustifyContentMode(JustifyContentMode.CENTER);
         setAlignItems(Alignment.CENTER);
 
-        MemoryBuffer buffer = new MemoryBuffer();
-        Upload upload = new Upload(buffer);
+        memoryBuffer = new MemoryBuffer();
+        informationContainer = new Span();
+        informationContainer.setText("Please upload your curriculum vitae in the container bellow:\n" +
+                "- click on the 'Upload' button;\n" +
+                "- browse for the file you want to upload;\n" +
+                "- click on the 'Upload CV' button bellow the container\n" +
+                "Only PDF files will be accepted");
 
-        upload.setAcceptedFileTypes("file/png");
-        upload.setMaxFiles(1);
-        upload.setDropLabel(new Label("Upload your CV in pdf format"));
+        uploadContainer = new Upload(memoryBuffer);
+        uploadButton = new Button();
 
-        add(upload);
+        uploadButton.addThemeVariants(MATERIAL_CONTAINED);
+        uploadButton.setText("Upload CV");
+        uploadButton.setWidth(uploadContainer.getWidth());
+
+        uploadContainer.setAcceptedFileTypes("file/png");
+        uploadContainer.setMaxFiles(1);
+        uploadContainer.setDropLabel(new Label("Upload your CV in pdf format"));
+        informationContainer.setWidth(uploadContainer.getWidth());
+        informationContainer.getStyle().set("font-size","14px");
+
+        add(informationContainer);
+        add(uploadContainer);
+        add(uploadButton);
     }
 }
